7e7dd4efcaab035a9b1ce67d5d43f073e8c3899f
anirban2404 <anirban_bhattacharjee@hotmail.com>
2017-08-14 15:48:25 -0500

    Second Assignment

--- Assignments_Matlab/machine-learning-ex2/ex2/costFunctionReg.m
+++ Assignments_Matlab/machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,8 +18,14 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+% excluded the first theta value
+theta1 = [0 ; theta(2:size(theta), :)];
+
+h = sigmoid(X*theta);
+J = 1/m * ((-y)'*log(h)-(1-y)'*log(1-h)) + lambda/(2 * m) * sum(theta1.^2);
 
 
+grad = 1/m * (X' * (h - y)) + lambda/m * theta1;
 
 
 % =============================================================
